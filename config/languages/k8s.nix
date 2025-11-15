4415aa4fd3c9fb3ee1a7a792e590225be61e0ed8
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-11-15 13:24:43 +0100

    improve k8s neovim experience

--- config/languages/k8s.nix
+++ config/languages/k8s.nix
@@ -1,4 +1,27 @@
-{_, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  extraPlugins = [
+    (
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "yaml-schema-detect";
+        src = pkgs.fetchFromGitHub {
+          owner = "cwrau";
+          repo = "yaml-schema-detect.nvim";
+          rev = "605f81b2dc3243b61017db7099a808e8ea350566";
+          hash = "sha256-RTblSvtz0/jbjWAe+MI4EXS87Q/lUlPyt/1E2yquXtY=";
+        };
+      }).overrideAttrs (oldAttrs: {
+        dependencies = with pkgs.vimPlugins; [
+          plenary-nvim
+          nvim-lspconfig
+        ];
+      })
+    )
+  ];
+  extraConfigLua = "require('yaml-schema-detect').setup()";
   plugins = {
     lsp = {
       enable = true;
@@ -8,10 +31,8 @@
           settings = {
             schemas = {
               kubernetes = "*.yaml";
-              "https://json.schemastore.org/gitlab-ci" = "*gitlab-ci*.{yml,yaml}";
-              "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*compose*.{yml,yaml}";
-              "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
-              "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
+              "https://json.schemastore.org/chart" = "Chart.{yml,yaml}";
+              "https://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
             };
           };
         };
