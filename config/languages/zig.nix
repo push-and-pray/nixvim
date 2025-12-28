83b0232114f4a7c5265fc62acb1a0288175fea7f
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-12-28 10:19:26 +0100

    update zig config

--- config/languages/zig.nix
+++ config/languages/zig.nix
@@ -1,12 +1,22 @@
 {_, ...}: {
   plugins = {
-    zig.enable = true;
     lsp = {
       enable = true;
       servers = {
         zls.enable = true;
       };
     };
+
+    conform-nvim = {
+      settings.formatters_by_ft = {
+        zig = ["zigfmt"];
+      };
+    };
+
+    lint.lintersByFt = {
+      zig = ["zlint"];
+    };
+
     neotest = {
       enable = true;
       adapters = {
