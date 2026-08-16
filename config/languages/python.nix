ad1d621549fcf46a9359de901210162e7293eb06
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-08-16 11:18:10 +0200

    enable dap python

--- config/languages/python.nix
+++ config/languages/python.nix
@@ -1,5 +1,39 @@
 {pkgs, ...}: {
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>dpr";
+      action.__raw = ''
+        function()
+          require("dap-python").test_method()
+        end
+      '';
+      options.desc = "Debug nearest Python test method";
+    }
+    {
+      mode = "n";
+      key = "<leader>dpc";
+      action.__raw = ''
+        function()
+          require("dap-python").test_class()
+        end
+      '';
+      options.desc = "Debug Python test class";
+    }
+  ];
   plugins = {
+    dap-python = {
+      enable = true;
+      resolvePython = ''
+        function()
+          local path = vim.fn.exepath("python3")
+          if path ~= "" then
+            return path
+          end
+          return nil
+        end
+      '';
+    };
     lsp = {
       enable = true;
       servers = {
